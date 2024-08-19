03b888e3c0330f1efc12d622af62b98803d5eae5
Ali Raza Ramejo <alirazakhan2540@gmail.com>
2024-08-19 11:24:39 +0500

    start a new project job portal and complete clerk authentication

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Online Job Portal",
+  description: "Create your own online job portal application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return ( <div>
+    <Button>Click me</Button>
+  </div> );
+}
+ 
+export default HomePage;
